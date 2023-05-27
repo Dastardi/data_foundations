fb6fbffd1721e47d7e7af82962476f7d293e359a
Luke Currier <lukescurrier@Gmail.com>
2023-05-27 10:41:05 -0700

    update

---
+++ HW2/crime_analysis.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+crime_18 = pd.read_csv('boston_crime_2018.csv')
+crime_20 = pd.read_csv('boston_crime_2020.csv')
+crime_22 = pd.read_csv('boston_crime_2022.csv')
+
